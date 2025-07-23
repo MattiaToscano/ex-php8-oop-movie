0c4f06c123bd4fed45388959ef0a8e594c43de9a
MattiaToscano <mattia1997ct@gmail.com>
2025-07-23 15:26:24 +0200

    [UPD] Implementata classe e funzione

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+class Film{
+
+    public $title;
+    public $genre;
+    public $year;
+
+    function __construct($title, $genre, $year)
+    {
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->year =$year;
+
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FILMZZZZ</title>
+</head>
+<body>
+    
+</body>
+</html>
